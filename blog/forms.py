feaaf6a892c4d4c682ae40483f714bbdd2aceff0
your Name <you@example.com>
2018-06-18 21:20:21 +0900

    hi!

--- blog/forms.py
+++ blog/forms.py
@@ -10,16 +10,26 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text',)
 
-class CreateUserForm(UserCreationForm): # 내장 회원가입 폼을 상속받아서 확장한다.
-    email = forms.EmailField(required=True) # 이메일 필드 추가
-    
+class UserForm(UserCreationForm):
+    이메일 = forms.EmailField(required=True)  # 이메일 필드 추가
+    이름 = forms.CharField(required=True)
+    닉네임 = forms.CharField(required=True)
+    전화번호 = forms.CharField(required=True)
+    학번 = forms.CharField(required=True)
+    성별 = forms.CharField(required=True)
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "password1", "password2", "이름", "닉네임", "전화번호", "학번", "성별", "이메일")
 
-    def save(self, commit=True): # 저장하는 부분 오버라이딩
-        user = super(CreateUserForm, self).save(commit=False) # 본인의 부모를 호출해서 저장하겠다.
-        user.email = self.cleaned_data["email"]
+
+    def save(self, commit=True):  # 저장하는 부분 오버라이딩
+        user = super(UserForm, self).save(commit=False)  # 본인의 부모를 호출해서 저장하겠다.
+        user.email = self.cleaned_data["이메일"]
+        user.realname = self.cleaned_data["이름"]
+        user.nickname = self.cleaned_data["닉네임"]
+        user.phone = self.cleaned_data["전화번호"]
+        user.stdi = self.cleaned_data["학번"]
+        user.gender = self.cleaned_data["성별"]
         if commit:
             user.save()
         return user
